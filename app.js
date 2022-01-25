a0d035b24fa0b05037d4eb91544872a1c29ba4ef
Matt <84802242+imattk@users.noreply.github.com>
2022-01-25 10:31:35 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+const swup = new Swup(); 
+
+function toggleVideo(){
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active')
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+
+
